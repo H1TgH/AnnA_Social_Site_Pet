98a02873c910baa17f9eb77f16e942fa070b3f59
Ivan Fedorov <ivan45sc@gmail.com>
2025-09-03 21:19:31 +0500

    Change redis url

--- src/celery_worker/app.py
+++ src/celery_worker/app.py
@@ -2,12 +2,13 @@ import os
 from celery import Celery
 
 
-redis_url = os.getenv('REDIS_URL', 'redis://redis:6379/0')
+redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
+redis_celery_url = f'{redis_url}/0'
 
 celery_app = Celery(
     'worker',
-    broker=redis_url,
-    backend=redis_url
+    broker=redis_celery_url,
+    backend=redis_celery_url
 )
 
 celery_app.conf.task_routes = {
--- src/email_service/app.py
+++ src/email_service/app.py
@@ -2,9 +2,10 @@ from celery import Celery
 import os
 
 
-redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
+redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
+redis_celery_url = f'{redis_url}/0'
 
-celery_app = Celery('worker', broker=redis_url, backend=redis_url)
+celery_app = Celery('worker', broker=redis_celery_url, backend=redis_celery_url)
 
 celery_app.conf.task_routes = {
     'src.email_service.tasks.send_confirmation_email_task': {'queue': 'email'},
